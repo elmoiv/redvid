da420149f5d1301a99e848f457709f31a5639af7
Khaled ElMorshedy <elmoiv@yahoo.com>
2022-03-09 15:13:05 +0200

    Updated to 1.2.0

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
+from redvid import __version__ as version
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 requirements = []
@@ -8,20 +9,26 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    name="redvid",
-    version="1.1.3",
-    author="elmoiv",
-    author_email="elmoiv@yahoo.com",
-    description="Smart downloader for Reddit hosted videos",
+    name='redvid',
+    version=version,
+    author='Khaled El-Morshedy',
+    license='GPL-v3.0',
+    author_email='elmoiv@yahoo.com',
+    description='Smart downloader for Reddit hosted videos',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/elmoiv/redvid",
+    long_description_content_type='text/markdown',
+    url='https://github.com/elmoiv/redvid',
     packages=setuptools.find_packages(exclude=['tests']),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.4',
+    entry_points={
+        'console_scripts': [
+            'redvid=redvid.__main__:run',
+        ],
+    },
 )
